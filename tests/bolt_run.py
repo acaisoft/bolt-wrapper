1766d0b2cae725df94356beb4ff6ac428f78f1ff
art.barysevich <art.barysevich@gmail.com>
2019-05-24 15:57:48 +0200

    Add stage logging

--- tests/bolt_run.py
+++ tests/bolt_run.py
@@ -1,10 +1,11 @@
+import datetime
 import json
 import sys
 import os
 import importlib
 import time
 
-from locust.main import main
+from locust.main import main as locust_main
 
 from bolt_exceptions import MonitoringExit
 from bolt_logger import setup_custom_logger
@@ -15,7 +16,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # envs
-WRAPPER_VERSION = '0.2.21'
+WRAPPER_VERSION = '0.2.15'
 GRAPHQL_URL = os.getenv('BOLT_GRAPHQL_URL')
 HASURA_TOKEN = os.getenv('BOLT_HASURA_TOKEN')
 EXECUTION_ID = os.getenv('BOLT_EXECUTION_ID')
@@ -37,6 +38,10 @@ logger.info(os.environ)
 # consts
 EXIT_STATUS_SUCCESS = 0
 EXIT_STATUS_ERROR = 1
+SCENARIO_TYPE: str
+
+no_keep_alive = True if WORKER_TYPE == 'slave' else False
+bolt_api_client = BoltAPIClient(no_keep_alive=no_keep_alive)
 
 
 def _exit_with_status(status):
@@ -44,43 +49,52 @@ def _exit_with_status(status):
     sys.exit(status)
 
 
-def _import_and_run(module_name, func_name='main', **kwargs):
+def _stage_log(msg, level='info'):
+    bolt_api_client.insert_execution_stage_log({
+        'timestamp': datetime.datetime.now().isoformat(), 'stage': SCENARIO_TYPE, 'level': level, 'msg': msg})
+
+
+def _import_and_run(scenario_type, module_name, func_name='main', **kwargs):
+    _stage_log(f'Starting {scenario_type}')
     try:
         module = importlib.import_module(module_name)
         func = getattr(module, func_name)
     except (ModuleNotFoundError, AttributeError) as ex:
-        logger.exception(f'Error during importing module/function. {ex}')
+        logger.exception(f'Import error | {ex}', level='error')
+        _stage_log(f'Could not import func/module | {ex}', level='error')
         _exit_with_status(EXIT_STATUS_ERROR)
     except Exception as ex:
-        logger.exception(f'Unknown exception during importing module/function for execution. Exception {ex}')
+        logger.exception(f'Unknown exception during importing module/function for execution | {ex}')
+        _stage_log(f'Could not import func/module | {ex}', level='error')
         _exit_with_status(EXIT_STATUS_ERROR)
     else:
+        _stage_log(f'Running ...')
         start_time = time.time()
         try:
             func(**kwargs)
         except MonitoringExit as ex:
             logger.exception(f'Caught exception during execution monitoring | {ex}')
+            _stage_log(f'Error during monitor execution | {ex}', level='error')
             _exit_with_status(EXIT_STATUS_ERROR)
         except Exception as ex:
             logger.exception(f'Caught unknown exception during execution | {ex}')
+            _stage_log(f'Error during execution | {ex}', level='error')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
-            total_time = time.time() - start_time
+            total_time = int(time.time() - start_time)
             logger.info(f'Successfully executed function {module_name}.{func_name}. Time execution {total_time} sec.')
+            _stage_log(f'{scenario_type.title()} successfully finished')
             _exit_with_status(EXIT_STATUS_SUCCESS)
 
 
 class Runner(object):
-    def __init__(self):
-        no_keep_alive = True if WORKER_TYPE == 'slave' else False
-        self.bolt_api_client = BoltAPIClient(no_keep_alive=no_keep_alive)
-
     @staticmethod
     def set_configuration_environments(data):
         try:
             configuration = data['execution'][0]['configuration']
         except LookupError as ex:
-            logger.info(f'Error during extracting test relations from database {ex}')
+            logger.exception(f'Error during extracting environments from configuration | {ex}')
+            _stage_log('Could not extract environments for configuration', level='error')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
             for envs in configuration.get('configuration_envvars', []):
@@ -92,21 +106,20 @@ class Runner(object):
         try:
             configuration = data['execution'][0]['configuration']
         except LookupError as ex:
-            logger.info(f'Error during extracting test relations from database {ex}')
+            logger.exception(f'Error during setting environments for load tests | {ex}')
+            _stage_log('Could not set environments for load tests', level='error')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
             if configuration['test_source']['source_type'] not in ('repository', 'test_creator'):
-                logger.info('Invalid source_type value.')
+                logger.info('Invalid source_type value')
                 _exit_with_status(EXIT_STATUS_ERROR)
             if configuration['test_source']['source_type'] == 'repository':
                 os.environ['BOLT_LOCUSTFILE_NAME'] = 'load_tests'
-                os.environ['BOLT_MIN_WAIT'] = '50'
-                os.environ['BOLT_MAX_WAIT'] = '100'
             elif configuration['test_source']['source_type'] == 'test_creator':
                 try:
                     test_creator = configuration['test_source']['test_creator']
                 except LookupError as ex:
-                    logger.info(f'Error during getting data for Test Creator {ex}')
+                    logger.exception(f'Error during getting data for Test Creator | {ex}')
                     _exit_with_status(EXIT_STATUS_ERROR)
                     return
                 os.environ['BOLT_LOCUSTFILE_NAME'] = 'locustfile_generic'
@@ -133,7 +146,7 @@ class Runner(object):
         try:
             configuration = data['execution'][0]['configuration']
         except LookupError as ex:
-            logger.info(f'Error during extracting test relations from database {ex}')
+            logger.exception(f'Error during checking that configuration has load tests | {ex}')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
             return configuration['has_load_tests']
@@ -141,21 +154,23 @@ class Runner(object):
     @staticmethod
     def get_monitoring_arguments(data):
         try:
-            configurations = data['execution'][0]['configuration']['configuration_parameters']
-            if not configurations:
+            parameters = data['execution'][0]['configuration']['configuration_parameters']
+            if not parameters:
                 raise LookupError('No arguments for configurations')
         except LookupError as ex:
-            logger.info(f'Error during extracting arguments from database {ex}')
+            logger.exception(f'Error during extracting arguments for monitoring from database | {ex}')
+            _stage_log('Occurred error during getting arguments for monitoring', level='error')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
             arguments = {}
-            for config in configurations:
-                if config['parameter_slug'] in ('monitoring_duration', 'monitoring_interval'):
-                    arguments[config['parameter_slug']] = config['value']
+            for p in parameters:
+                parameter_slug = p['parameter_slug']
+                if parameter_slug.startswith('monitoring_'):
+                    arguments[parameter_slug] = p['value']
             return arguments
 
     @staticmethod
-    def get_locust_arguments(data, extra_arguments):
+    def get_load_tests_arguments(data, extra_arguments):
         argv = sys.argv or []
         # delete `load_tests` argument from list of argv's
         try:
@@ -164,20 +179,20 @@ class Runner(object):
             pass
         # preparing arguments for locust
         try:
-            configurations = data['execution'][0]['configuration']['configuration_parameters']
-            if not configurations:
+            parameters = data['execution'][0]['configuration']['configuration_parameters']
+            if not parameters:
                 raise LookupError('No arguments for configurations')
         except LookupError as ex:
-            logger.info(f'Error during extracting arguments from database {ex}')
+            logger.exception(f'Error during extracting arguments for locust from database {ex}')
+            _stage_log('Occurred error during getting arguments for load tests', level='error')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
             argv.extend(['-f', 'bolt_locust_wrapper.py'])
-            # get and put arguments from database
-            for config in configurations:
-                # TODO: need refactoring
-                if config['parameter']['param_name'] in ('-md', '-mi'):
-                    continue
-                argv.extend([config['parameter']['param_name'], config['value']])
+            # get and put locust arguments from database
+            for p in parameters:
+                parameter_slug = p['parameter_slug']
+                if parameter_slug.startswith('load_tests_'):
+                    argv.extend([p['parameter']['param_name'], p['value']])
             argv.extend(['--no-web'])
             argv.extend(['--csv=test_report'])
             if extra_arguments is not None:
@@ -190,24 +205,17 @@ class Runner(object):
         :return: is_pre_start: bool | is_post_stop: bool | is_monitoring: bool | is_load_tests: bool
         """
         try:
-            scenario_type = sys.argv[1]
+            scenario = sys.argv[1]
         except IndexError:
-            logger.info(f'Scenario type does not found. Args {sys.argv}')
+            logger.exception(f'Scenario type does not found. Args {sys.argv}')
             _exit_with_status(EXIT_STATUS_ERROR)
         else:
             logger.info(f'Trying to detect scenario from arguments {sys.argv}')
-            if scenario_type == 'pre_start':
-                logger.info('Detected `pre_start` scenario')
-                return True, False, False, False
-            elif scenario_type == 'post_stop':
-                logger.info('Detected `post_stop` scenario')
-                return False, True, False, False
-            elif scenario_type == 'monitoring':
-                logger.info('Detected `monitoring` scenario')
-                return False, False, True, False
-            elif scenario_type == 'load_tests':
-                logger.info('Detected `load_tests` scenario')
-                return False, False, False, True
+            if scenario in ('pre_start', 'post_stop', 'monitoring', 'load_tests'):
+                logger.info(f'Detected scenario {scenario}')
+                global SCENARIO_TYPE  # additional set scenario as global variable
+                SCENARIO_TYPE = scenario
+                return scenario
             else:
                 logger.info('Unknown scenario ...')
                 _exit_with_status(EXIT_STATUS_ERROR)
@@ -226,32 +234,36 @@ class Runner(object):
 
     def prepare_master_arguments(self, expect_slaves):
         logger.info(f'Start preparing arguments for master.')
-        self.bolt_api_client.insert_execution_instance({
+        bolt_api_client.insert_execution_instance({
             'status': 'READY', 'instance_type': WORKER_TYPE, 'expect_slaves': expect_slaves})
         return ['--master', f'--expect-slaves={expect_slaves}']  # additional arguments for master
 
     def prepare_slave_arguments(self):
         logger.info(f'Start preparing arguments for slave.')
-        self.bolt_api_client.insert_execution_instance({
+        bolt_api_client.insert_execution_instance({
             'host': MASTER_HOST, 'port': 5557, 'status': 'READY', 'instance_type': WORKER_TYPE})
         return ['--slave', f'--master-host={MASTER_HOST}']  # additional arguments for slave
 
 
-if __name__ == '__main__':
+def main():
     runner = Runner()
-    execution_data = runner.bolt_api_client.get_execution(execution_id=EXECUTION_ID)
+    scenario_type = runner.scenario_detector()
+    _stage_log('Initialization')
+    execution_data = bolt_api_client.get_execution(execution_id=EXECUTION_ID)
     runner.set_configuration_environments(execution_data)
-    is_pre_start, is_post_stop, is_monitoring, is_load_tests = runner.scenario_detector()
-    if is_pre_start:
-        _import_and_run('bolt_flow.pre_start')
-    elif is_post_stop:
-        _import_and_run('bolt_flow.post_stop')
-    elif is_monitoring:
+    if scenario_type == 'pre_start':
+        _import_and_run(scenario_type, 'bolt_flow.pre_start')
+    elif scenario_type == 'post_stop':
+        _import_and_run(scenario_type, 'bolt_flow.post_stop')
+    elif scenario_type == 'monitoring':
         monitoring_arguments = runner.get_monitoring_arguments(execution_data)
         has_load_tests = runner.has_load_tests(execution_data)
         _import_and_run(
-            'bolt_monitoring_wrapper', has_load_tests=has_load_tests, monitoring_arguments=monitoring_arguments)
-    elif is_load_tests:
+            scenario_type, 'bolt_monitoring_wrapper',
+            has_load_tests=has_load_tests, monitoring_arguments=monitoring_arguments
+        )
+    elif scenario_type == 'load_tests':
+        _stage_log('Starting load test')
         runner.set_environments_for_load_tests(execution_data)
         # master/slave
         additional_arguments = None
@@ -263,8 +275,14 @@ if __name__ == '__main__':
             additional_arguments = runner.prepare_slave_arguments()
         # set arguments to locust
         logger.info(f'Arguments (sys.argv) before {sys.argv}')
-        sys.argv = runner.get_locust_arguments(execution_data, additional_arguments)
+        sys.argv = runner.get_load_tests_arguments(execution_data, additional_arguments)
         logger.info(f'Arguments (sys.argv) after {sys.argv}')
         # monkey patch for returning 0 (success) status code
         sys.exit = lambda status: None
-        main()  # test runner
+        _stage_log('Running ...')
+        locust_main()  # locust test runner
+        _stage_log('Load test successfully finished')
+
+
+if __name__ == '__main__':
+    main()
