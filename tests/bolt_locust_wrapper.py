1138af9255e846c0861f1f133bde3600d7125379
art.barysevich <art.barysevich@gmail.com>
2019-07-18 19:16:33 +0200

    Revert. Hide supervisor

--- tests/bolt_locust_wrapper.py
+++ tests/bolt_locust_wrapper.py
@@ -196,32 +196,32 @@ class LocustWrapper(object):
 
     def push_event(self, data, event_type):
         # extracting errors for common cases (when WORKER_TYPE is not 'master' or 'slave')
-        if event_type == 'failure':
-            combined_key = '{0}/{1}/{2}'.format(data['request_type'], data['endpoint'], data['exception'])
-            # combined_key = wrap_re.sub(r' object at 0x\S*', '', combined_key)  # delete trash (obj address) from key
-            try:
-                error = self.errors[combined_key]
-                error['number_of_occurrences'] = error['number_of_occurrences'] + 1
-            except KeyError:
-                new_error = {combined_key: {
-                    'execution_id': self.execution, 'number_of_occurrences': 1, 'name': data['endpoint'],
-                    'error_type': data['request_type'], 'exception_data': data['exception']
-                }}
-                self.errors.update(new_error)
-        # extracting errors when WORKER_TYPE is 'master'
-        elif event_type == 'master' and 'errors' in data.keys() and data['errors']:
-            for error in data['errors'].values():
-                combined_key = '{0}/{1}/{2}'.format(error['method'], error['name'], error['error'])
-                # combined_key = wrap_re.sub(r' object at 0x\S*', '', combined_key)  # delete trash (obj address) from key
-                try:
-                    _error = self.errors[combined_key]
-                    _error['number_of_occurrences'] += error['occurences']
-                except KeyError:
-                    new_error = {combined_key: {
-                        'execution_id': self.execution, 'number_of_occurrences': error['occurences'],
-                        'name': error['name'], 'error_type': error['method'], 'exception_data': error['error']
-                    }}
-                    self.errors.update(new_error)
+        # if event_type == 'failure':
+        #     combined_key = '{0}/{1}/{2}'.format(data['request_type'], data['endpoint'], data['exception'])
+        #     combined_key = wrap_re.sub(r' object at 0x\S*', '', combined_key)  # delete trash (obj address) from key
+        #     try:
+        #         error = self.errors[combined_key]
+        #         error['number_of_occurrences'] = error['number_of_occurrences'] + 1
+        #     except KeyError:
+        #         new_error = {combined_key: {
+        #             'execution_id': self.execution, 'number_of_occurrences': 1, 'name': data['endpoint'],
+        #             'error_type': data['request_type'], 'exception_data': data['exception']
+        #         }}
+        #         self.errors.update(new_error)
+        # # extracting errors when WORKER_TYPE is 'master'
+        # elif event_type == 'master' and 'errors' in data.keys() and data['errors']:
+        #     for error in data['errors'].values():
+        #         combined_key = '{0}/{1}/{2}'.format(error['method'], error['name'], error['error'])
+        #         combined_key = wrap_re.sub(r' object at 0x\S*', '', combined_key)  # delete trash (obj address) from key
+        #         try:
+        #             _error = self.errors[combined_key]
+        #             _error['number_of_occurrences'] += error['occurences']
+        #         except KeyError:
+        #             new_error = {combined_key: {
+        #                 'execution_id': self.execution, 'number_of_occurrences': error['occurences'],
+        #                 'name': error['name'], 'error_type': error['method'], 'exception_data': error['error']
+        #             }}
+        #             self.errors.update(new_error)
         # push event to dataset for common cases
         last_timestamp = list(self.dataset[-1].keys())[0]
         now_timestamp = wrap_time.time()
