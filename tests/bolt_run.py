ba7e435c3e7dc28fcc6a2808ed7cda1b58d6fbd5
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2022-05-09 09:25:45 +0200

    - (debug) deleting branch argument

--- tests/bolt_run.py
+++ tests/bolt_run.py
@@ -170,6 +170,8 @@ class Runner(object):
         for e in data['execution'][0]['configuration']['configuration_parameters']:
             if e['parameter']['param_name'] == '-c':
                 e['parameter']['param_name'] = '-u'
+            if e['parameter']['param_name'] == '-b':
+                del e
         try:
             argv.remove('load_tests')
         except ValueError:
